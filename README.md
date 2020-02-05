1ea01ee0dec827cf07f2e7ec120e193c8e194f2e
diegomrz <diegomrz@outlook.com>
2020-02-05 00:07:32 -0300

    First commit, GoStack - Desafio1

---
+++ README.md
@@ -0,0 +1,31 @@
+<h1 align="center">
+  GoStack Desafio 1 : Conceitos do NodeJS
+</h1>
+
+<h2 align="center">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/diegomrz/gostackdesafio1">
+
+  <a href="https://www.linkedin.com/in/diegomrz/">
+    <img alt="Made by diegomrz" src="https://img.shields.io/badge/linkedin-diegomrz-blue">
+  </a>
+
+  <a href="https://github.com/Rocketseat/bootcamp-gostack-desafio-01/blob/master/README.md#desafio-01-conceitos-do-nodejs">
+    <img alt="Desafio" src="https://img.shields.io/badge/desafio-Rocketseat-blueviolet">
+  </a>
+</h2>
+
+
+## Instrução do desafio
+Crie uma aplicação para armazenar projetos e suas tarefas do zero utilizando [Express](https://expressjs.com/pt-br/).
+
+### Rotas a criar
+
+- `POST /projects`: Criar um novo projeto. [ WORKING ]
+
+- `GET /projects`: Listar todos os projetos. [ WORKING ]
+
+- `PUT /projects/:id`: Alterar um projeto. [ WORKING ]
+
+- `DELETE /projects/:id`: Deletar um projeto. [ WORKING ]
+
+- `POST /projects/:id/tasks`: Criar uma task dentro de um projeto existente. [ DEVELOPING ]
---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+//Dados
+const projects = [
+            ];
+
+            //Middlewares
+server.use((req, res, next) => {
+  console.count("Requisições");
+  console.log('Middleware global - Requisição chamada.');
+  console.log(`Método: ${req.method}, URL: ${req.url}`);
+  return next();
+});
+
+function checkIfProjectExists(req, res, next){
+  const { id } = req.params;
+  const project = projects.find(p => p.id == id);
+  if (!project) {
+    return res.status(400).json({ error: 'Project not found' });
+  }
+  return next();
+}
+
+//Routes
+server.get('/projects', (req, res) => {
+  return res.json(projects);
+});
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+  projects.push(project);
+
+  return res.json(project);
+});
+
+server.get('/projects/:index', (req, res) => {
+  return res.json(req.projects);
+});
+
+server.put('/projects/:id', checkIfProjectExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project);
+});
+
+server.delete('/projects/:id', (req, res) => {
+  const { id } = req.params;
+  const index = projects.findIndex(p => p.id == id);
+  projects.splice(index, 1);
+
+  return res.send();
+});
+
+server.listen(3000);
